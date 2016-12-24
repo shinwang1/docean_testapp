47cc5a92e3002d21a0a5fbfbec2c27229e1513b0
Shin <mrmaxin@gmail.com>
2016-12-23 22:42:23 -0800

    add root servers#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'servers#index'
+  
   resources :servers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

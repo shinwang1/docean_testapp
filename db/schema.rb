2b0ad38a10c8f05ea4eb587fac409aff51f4abb5
Shin <mrmaxin@gmail.com>
2016-12-24 00:17:05 -0800

    minor change to db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161224063830) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "servers", force: :cascade do |t|
     t.string   "name"
     t.string   "ip_address"
